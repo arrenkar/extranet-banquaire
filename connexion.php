aac7523b7c9eb40ca1e121930462a17b806f5fc7
Benoit Fiarda <fiarda33@live.fr>
2022-06-13 10:03:35 +0200

    Ajout du fichier connexion.php

---
+++ connexion.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once 'config.php';
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+
+    $check = $bdd->prepare('SELECT pseudo, email, password FROM extranet WHERE email = ?');
+    $check->execute(array($email));
+    $data = $check->fetch();
+    $row = $check->rowCount();
+
+    if ($row == 1) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $password = hash('sha256', $password);
+            if ($data['password'] === $password) {
+                $_SESSION['user'] = $data['pseudo'];
+                header('Location:landing.php');
+            } else header('Location:index.php?login_error=password');
+        } else header('Location:index.php?login_error=email');
+    } else header('Location:index.php?login_error=already');
+} else header('Location:index.php');
